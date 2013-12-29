7f6529b7cc4ad3502f97a9a847cc37b9169f9019
towaywu <bboyer@qq.com>
2013-12-30 00:24:00 +0800

    编写了tQuery.js的头注释

--- WebContent/js/tQuery.js
+++ WebContent/js/tQuery.js
@@ -1,3 +1,8 @@
+/**
+ * tQuery是参照jQuery的写法编写的<br>
+ * 之所以要编写这个框架，主要是在实际项目中遇到了jquery处理起来不方便的功能；
+ * 如果扩展jQuery的方法的话，容易忘掉。
+ */
 $(function(win,doc){
 	
 	var tQuery = function(){
@@ -10,6 +15,9 @@ $(function(win,doc){
 			}
 	}
 	
+	/**
+	 * 扩展方法
+	 */
 	tQuery.extend = function(){
 		
 	}	
