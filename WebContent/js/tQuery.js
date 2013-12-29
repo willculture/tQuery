16bb0954c06c172f43f38dfa2ea16cd9249376f2
towaywu <bboyer@qq.com>
2013-12-29 22:56:45 +0800

    first

---
+++ WebContent/js/tQuery.js
@@ -0,0 +1,19 @@
+$(function(win,doc){
+	
+	var tQuery = function(){
+		return new tQuery.fn.init();
+	}
+	
+	tQuery.fn = {
+			init:function(){
+				
+			}
+	}
+	
+	tQuery.extend = function(){
+		
+	}	
+	
+	
+	
+})(window,document);
