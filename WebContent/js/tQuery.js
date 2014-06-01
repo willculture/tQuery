a3ed7f1342699952d157b032a8271eb00074f137
towaywu <bboyer@qq.com>
2014-06-02 07:58:00 +0800

    修改了IE中的bug

--- WebContent/js/tQuery.js
+++ WebContent/js/tQuery.js
@@ -12,23 +12,32 @@
 	tQuery.fn = tQuery.prototype = {
 		length : 0,
 		constructor : tQuery,
+		/**
+		 * 初始化，主要是选择器判断 <br />
+		 * 1. # 代表属性id<br />
+		 * 2. . 代表属性class<br />
+		 * 3. tag 代表标签
+		 */
 		init : function(selector) {
 
-			if (selector.constructor == tQuery) {
-				return selector;
-			} else if (tQuery.utils.startWith(selector, "#")) {
+			if (selector == undefined) {
+				return;
+			}
 
+			//
+			if (selector.constructor == tQuery) {// 如果是tQuery 
+				return selector;
+			} else if (tQuery.utils.startWith(selector, "#")) { // 如果是id
 				var ele = tQuery.utils.removeStart(selector, "#");
 				this.length = 1;
 				var node = document.getElementById(ele);
 				this[0] = node;
 
-			} else if (tQuery.utils.startWith(selector, ".")) {
-
+			} else if (tQuery.utils.startWith(selector, ".")) {// 如果是class 
 				var elelist = document.getElementsByTagName("*");
 				var rootname = tQuery.utils.removeStart(selector, ".");
 				this.length = 0;
-				for ( var i = 0; i < elelist.length; i++) {
+				for (var i = 0; i < elelist.length; i++) {
 					var classname = elelist[i].getAttribute("class");
 					if (classname && classname.indexOf(rootname) >= 0) {
 						this[this.length] = elelist[i];
@@ -36,7 +45,13 @@
 					}
 				}
 
-			} else {
+			} else {// 如果是标签名 
+				var elelist = document.getElementsByTagName(selector);
+				this.length = 0;
+				for (var i = 0;i<elelist.length;i++) {
+					this[this.length] = elelist[i]; 
+					this.length++;
+				}
 
 			}
 			return this;
@@ -44,14 +59,16 @@
 		size : function() {
 			return this.length;
 		},
-
 		html : function() {
 			return this[0].innerHTML;
+		},
+		outHtml:function(){
+			return this[0].outerHTML;
 		}
 
 	}
 	/**
-	 * 
+	 * 工具方法
 	 */
 	tQuery.utils = {
 		/**
@@ -59,13 +76,14 @@
 		 */
 		startWith : function(source, param) {
 			var len = param.length;
-			var b = true;
-			for ( var i = 0; i < len; i++) {
-				if (param[i] != source[i]) {
+			var b = true; 
+			for (var i = 0; i < len; i++) {
+				 
+				if (param.charCodeAt(i) != source.charCodeAt(i)) {
 					b = false;
 					break;
 				}
-			}
+			} 
 			return b;
 		},
 		/**
@@ -85,20 +103,26 @@
 		}
 	}
 
+	/**
+	 * 扩展选择器 <br />
+	 * eq:从列表中选择 <br />
+	 * find:从所有子类中选择 <br />
+	 * parent:从当前父类中选择 <br />
+	 */
 	tQuery.fn.extend({
 		eq : function(index) {
 			var t = this.constructor();
-			t[0] = this[index];
+			t[0] = this[index];  
 			return t;
 		},
 		find : function(elem) {
 			var t = this.constructor(elem);
 			return t;
 		},
-		parent : function() {
-			var t = this.constructor();
+		parent : function() { 
+			var t = this.constructor();  
 			t[0] = this[0].parentNode;
-			t.length = 1;
+			t.length = 1; 
 			return t;
 		},
 		next : function() {
